02dd393177efeb078df1eaa1aa5b17b513026979
yoyo-a11 <yoyol264@deltalearns.ca>
2021-02-09 09:09:30 -0800

    updated all files

--- assignment.py
+++ assignment.py
@@ -1,3 +1,5 @@
 #! python3
 
 # SD Computing Studies Assignment
+
+print("Hello world!")
--- assignment3.py
+++ assignment3.py
@@ -3,5 +3,5 @@
 #  Use of the the print() and print( , end="") commands to display output.
 #  Check the README.md file for the sample output
 
-print("Hello world!")
-print("This is how multiple print statements are joined.")
+print("Hello world! This is how multiple print statements are jioned. Use end=\"\\",end="")
+
