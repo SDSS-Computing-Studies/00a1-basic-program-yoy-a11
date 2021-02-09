89b57b42c584f6b2d87e4e825c8a54d7763d15cf
yoyo-a11 <yoyol264@deltalearns.ca>
2021-02-09 09:35:59 -0800

    assignments 00a1

--- assignment3.py
+++ assignment3.py
@@ -3,4 +3,4 @@
 #  Use of the the print() and print( , end="") commands to display output.
 #  Check the README.md file for the sample output
 
-print("Hello world! This is how multiple print statements are jioned. Use end=\"\",end="")
+print("Hello world! This is how multiple print statements are joined. Use end=\"\"",end="")
